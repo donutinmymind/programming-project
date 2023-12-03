8b153fb9dcb04d80d0931c4ae2b169ce037a5db1
donutinmymind <nirvaan.parambat@gmail.com>
2023-12-03 22:08:53 +0000

    whiteboard

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk()
+window.title("Drawing Calculator")
+# default size and position
+window.geometry("300x300+100+100")
+# minimum size
+window.minsize(200, 200)
+# maximum size
+window.maxsize(500, 500)
+window.mainloop()
