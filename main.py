cd47a9eef5e56de9ed86510e9f7fa48157c10818
donutinmymind <151836065+donutinmymind@users.noreply.github.com>
2023-12-04 23:03:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tkinter
 from tkinter import *
-
 window = Tk()
 window.title("Drawing Calculator")
 # default size and position
@@ -8,4 +8,8 @@ window.geometry("300x300+100+100")
 window.minsize(200, 200)
 # maximum size
 window.maxsize(500, 500)
+# making the whiteboard border
+my_canvas = Canvas(window,width=100, height=100, bg="white")
+my_canvas.grid(columnspan=10)
+my_canvas.pack(pady=10)
 window.mainloop()
