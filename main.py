9c410e6c456e9952e74fa8f5a3036b5f4b1cc417
donutinmymind <151836065+donutinmymind@users.noreply.github.com>
2023-12-05 13:24:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-import tkinter
 from tkinter import *
+from tkinter.ttk import *
 window = Tk()
 window.title("Drawing Calculator")
 # default size and position
-window.geometry("300x300+100+100")
+window.geometry("300x300")
 # minimum size
 window.minsize(200, 200)
 # maximum size
 window.maxsize(500, 500)
 # making the whiteboard border
-my_canvas = Canvas(window,width=100, height=100, bg="white")
-my_canvas.grid(columnspan=10)
-my_canvas.pack(pady=10)
-window.mainloop()
+my_canvas = Canvas(window,width=300, height=250, bg="white")
+#equation produced after input made
+#sample holder
+equation = Label(window,text="7+8")
+equation.place(x=140,y=10)
+#label will be constant
+equal = Label(window,text="=")
+equal.place(x=180,y=10)
+#produce result of the equation
+equation = Label(window,text="15")
+equation.place(x=200,y=10)
+my_canvas.place(x=0,y=50)
+window.mainloop
