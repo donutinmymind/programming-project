ef6b272d8d5adee8d093425b63f572cd29328df9
donutinmymind <151836065+donutinmymind@users.noreply.github.com>
2023-12-13 12:41:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ window.minsize(200, 200)
 # maximum size
 window.maxsize(500, 500)
 # making the whiteboard window
-#The "ink"
+
 def get_x_and_y(event):
     global lasx, lasy
     lasx,lasy =event.x, event.y
@@ -17,12 +17,12 @@ def draw_smth(event):
     global lasx, lasy
     my_canvas.create_line((lasx, lasy,event.x, event.y),width=10,capstyle=ROUND,smooth=TRUE)
     lasx,lasy=event.x,event.y
-#The canvas    
+    
 Grid.columnconfigure(window, index = 0,weight = 1)
 Grid.rowconfigure(window, index=0, weight = 1)
 my_canvas=Canvas(window,width=300, height=250, bg="white")
 my_canvas.grid(row=0,column=0,sticky="NSEW")
-#The pen
+
 my_canvas.bind("<Button-1>",get_x_and_y)
 my_canvas.bind("<B1-Motion>", draw_smth)
 #equation produced after input made
@@ -35,4 +35,12 @@ equal.place(relx=0.50,y=10,anchor='n')
 #produce result of the equation
 answer = Label(window,text="15")
 answer.place(relx=0.60,y=10,anchor='n')
+# resizes labels based on window size
+def on_window_resize(event):
+    relwidth=window.winfo_width()
+    height=window.winfo_height()
+    equation.config(font=("Helvetica",int(relwidth/15)))
+    equal.config(font=("Helvetica",int(relwidth/15)))
+    answer.config(font=("Helvetica",int(relwidth/15)))
+window.bind("<Configure>",on_window_resize)
 window.mainloop
